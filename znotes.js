4180eb091d7a897426598235aa56a59359d9f254
Glitch (free-faithful-hellebore) <none>
2020-12-10 00:42:39 +0000

    ⛈🔖 Updated with Glitch

---
+++ znotes.js
@@ -0,0 +1,30 @@
+/* I am just going to write all my notes down in this file -Cindy */
+
+/* Index column ids 
+h col-xl: for the header
+h row: for the header
+c col-xl: for the cards
+c row: for the cards
+f col-xl: for the footer
+f row-xl: for the footer
+s row: for adding a padding and/or space
+h2.header: for the header titles
+ss row: same as s row but half as big
+*/
+
+/*Google maps for historic sites */
+/*
+Chinatown 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6305.654634805179!2d-122.41150892361479!3d37.79408646902385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808b58a58a1d%3A0x289f3092660f80ac!2sChinatown%2C%20San%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1607391912263!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Coit Tower 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.4725469886635!2d-122.40801088554193!3d37.802399118344304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808c40000001%3A0xde85b80121f2dd44!2sCoit%20Tower!5e0!3m2!1sen!2sus!4v1606955051519!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Golden Gate Bridge
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.724019518805!2d-122.48044378557333!3d37.819932817334816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808586deffffffc3%3A0xcded139783705509!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1605749453978!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Alcatraz
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.434744533073!2d-122.42517748557304!3d37.82670701694459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085811569574f23%3A0x189c28e85a71ae17!2sAlcatraz%20Island!5e0!3m2!1sen!2sus!4v1605749524047!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Golden Gate Park
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30935.108911186966!2d-122.49670964448862!3d37.76738517726881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808587726045f763%3A0x71be8b68ef3ba730!2sGolden%20Gate%20Park!5e0!3m2!1sen!2sus!4v1605749575080!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Palace of Fine Arts
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4440.460540645796!2d-122.45186370865858!3d37.801770312684695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808586d510144681%3A0xe960db8a581aacf5!2sThe%20Palace%20Of%20Fine%20Arts!5e0!3m2!1sen!2sus!4v1605749627045!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+*/
+
